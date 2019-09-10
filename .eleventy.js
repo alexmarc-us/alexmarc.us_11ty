53745bdaeb7d7c731e3b6846577bc0ce0fb8c53d
Alex Marcus <mr@alexmarc.us>
2019-09-10 08:08:38 -0700

    Adds date and source support, updates CSS to remove 'flows' class

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const moment = require('moment');
+moment.locale('en');
+
 module.exports = function(eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy('css')
@@ -7,6 +10,14 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addLayoutAlias('main', 'main.njk');
 
 	eleventyConfig.addLayoutAlias('post', 'post.njk');
+
+	eleventyConfig.addFilter('dateIso', date => {
+		return moment(date).toISOString();
+	});
+
+	eleventyConfig.addFilter('dateReadable', date => {
+		return moment(date).format('MMMM Do YYYY [around] h:mm a');
+	});
 
 	return {
 		passthroughFileCopy: true
