9d4e5f1dccf5053c571276f25e6141799b810a66
Alex Marcus <mr@alexmarc.us>
2019-09-30 08:04:56 -0700

    Sets up basic 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const fs = require("fs");
 moment.locale('en');
 
 module.exports = function(eleventyConfig) {
@@ -19,6 +20,20 @@ module.exports = function(eleventyConfig) {
 
 	eleventyConfig.addFilter('dateReadable', date => {
 		return moment(date).format('MMMM Do, YYYY [around] h:mm a');
+	});
+
+	eleventyConfig.setBrowserSyncConfig({
+		callbacks: {
+			ready: function(err, browserSync) {
+				const content_404 = fs.readFileSync('_site/404.html');
+
+				browserSync.addMiddleware("*", (req, res) => {
+					// Provides the 404 content without redirect.
+					res.write(content_404);
+					res.end();
+				});
+			}
+		}
 	});
 
 	return {
