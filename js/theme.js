ad671410ed3830978f355aa89cfe025db643f71f
Alex Marcus <mr@alexmarc.us>
2019-10-11 17:34:39 -0700

    Resolves flashing style with DOMContentLoaded, reorganizes theme code

--- js/theme.js
+++ js/theme.js
@@ -1,53 +1,65 @@
-window.onload = function() {
+/**
+ * Functionality related to theme setting and preference storage
+ */	
+
+// Removes all classes matching the parameter classMatch from body
+function removeBodyClassMatch (classMatch) {
+	var curClasses = document.body.classList;
+
+	// Check for all matching classes
+	for (var i = 0; i < curClasses.length; i++) {
+		// Remove matching classes
+		if ( curClasses[i].includes(classMatch) ) {
+			document.body.classList.remove(curClasses[i]);
+		}
+	}
+
+}
+
+// Sets the current body theme to theme parameter
+function setTheme(themeName) {
+	removeBodyClassMatch("theme-");
+
+	document.body.classList.add("theme-" + themeName);
+
+	// Store selection in localstorage
+	localStorage.setItem("theme", themeName);
+}
+
+// Sets the current body themeColor to theme color parameter
+function setThemeColor(themeColorName) {
+	removeBodyClassMatch("theme-color-");
+
+	document.body.classList.add("theme-color-" + themeColorName);
+
+	// Store selection in localstorage
+	localStorage.setItem("themeColor", themeColorName);
+}
+
+
+
+// Once the DOM is loaded but before images, styles, etc. load
+window.addEventListener('DOMContentLoaded', event => {
 	// Load theme selection from localstorage
-	loadThemePrefs();
+	var themePref = localStorage.getItem("theme");
+	var themeColorPref = localStorage.getItem("themeColor");
+
+	if (themePref) setTheme(themePref);
+	if (themeColorPref) setThemeColor(themeColorPref);
 
 	// Enable theme selection on change
 	var themeSelector = document.getElementById("theme-select");
 	var themeColorSelector = document.getElementById("theme-color-select");
-	if (themeSelector) themeSelector.onchange = setTheme;
-	if (themeColorSelector) themeColorSelector.onchange = setThemeColor;
+	if (themeSelector) {
+		themeSelector.onchange = () => setTheme(themeSelector.value);
 
-	function loadThemePrefs() {
-		
+		// Set theme selector to stored value, or default
+		themeSelector.value = themePref || "";
 	}
+	if (themeColorSelector) {
+		themeColorSelector.onchange = () => setThemeColor(themeColorSelector.value);
 
-
-	// Sets the current body theme to themeSelector value
-	function setTheme() {
-		var themeName = themeSelector.value;
-		
-		removeBodyClassMatch("theme-");
-
-		document.body.classList.add("theme-" + themeName);
-
-		// Store selection in localstorage
-		localStorage.setItem("theme", themeName);
+		// Set theme color selector to stored value, or default
+		themeColorSelector.value = themeColorPref || "";
 	}
-
-	// Sets the current body themeColor to themeColorSelector value
-	function setThemeColor() {
-		var themeColorName = themeColorSelector.value;
-		
-		removeBodyClassMatch("theme-color-");
-
-		document.body.classList.add("theme-color-" + themeColorName);
-
-		// Store selection in localstorage
-		localStorage.setItem("themeColor", themeColorName);
-	}
-		
-	// Removes all classes matching the parameter classMatch from body
-	function removeBodyClassMatch (classMatch) {
-		var curClasses = document.body.classList;
-
-		// Check for all matching classes
-		for (var i = 0; i < curClasses.length; i++) {
-			// Remove matching classes
-			if ( curClasses[i].includes(classMatch) ) {
-				document.body.classList.remove(curClasses[i]);
-			}
-		}
-
-	}
-}
+});
